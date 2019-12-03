46dd8d3893070ad996e510092ace2f964fd19698
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 14:43:13 -1000

    document form validation functions

--- src/formValidations/validateSearchInput.js
+++ src/formValidations/validateSearchInput.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const validateSearchInput = formValues => {
 	const errors = {};
 
+	// If the query is empty or users do not type anything in it before attempting to search, creator error message
 	if( !formValues.query || formValues.query === "" ){
 		errors.query = "Please enter a search term."
 	}
@@ -10,6 +11,9 @@ export const validateSearchInput = formValues => {
 	return errors;
 }
 
+// Redux-form will give us an object with meta fields error and touched if the field produces an error as defined in the form validation function and if the field has been touched
+
+// If the field has been touched and there is an error, we will render the error
 export const renderError = ({ error, touched }) => {
 	if( touched && error ){
 		return(
