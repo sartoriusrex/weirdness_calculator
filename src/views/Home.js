ff54cf4142eb002657f9c066c1f5dc94d22a83ae
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 20:52:43 -1000

    install tailwindcss and its dependencies; update scripts to build css files and serve during production as well as for dev server; configure postcss in config file; import tailwindcss in tailwind.css file and confirm use with className in Home page; generate tailwind.js file

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,7 +6,7 @@ import LikesContainer from '../components/LikesContainer';
 
 const Home = () => {
 	return(
-		<main>
+		<main className="container mx-auto">
 			<h1>Weirdness Calculator</h1>
 			<p>Find out how weird you are by selecting the GIFs that make you laugh. We will show the least weird ones to start. To change the weirdness of the results, you can move the slider.</p>
 			<p>When you find a GIF you like, press the <em>Like</em> button. Once you like 5 GIFs, you can calculate your weirdness.</p>
