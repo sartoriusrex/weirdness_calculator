e1029b0172bf2ac0c1f82f82415abf3dd1654cd2
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 14:56:46 -1000

    document LikesList

--- src/components/LikesList.js
+++ src/components/LikesList.js
@@ -6,10 +6,6 @@ import { unlikeGif } from '../store/actions/likes';
 const LikesList = props => {
 	const { likes, unlikeGif, from } = props;
 
-	function deleteGifFromList( gifSrc ){
-		unlikeGif( gifSrc );
-	}
-
 	return(
 		<ul>
 			{ likes.map( gif => 
@@ -17,13 +13,15 @@ const LikesList = props => {
 					<h4>{ gif.title }</h4>
 					<img 
 						src={ gif.stillSrc } 
-						alt={ gif.title } 
+						alt={ gif.title ? gif.title : "Title and description missing." } 
 					/>
+
+					{/* "from" prop passed down from Parent. If Results Page is parent, show weirdness score and not the delete button */}
 					{ from === "results" ? 
 						<p>{ gif.weirdness } / 10</p>
 						:
 						<button
-							onClick={ () => deleteGifFromList( gif.gifSrc ) }
+							onClick={ () => unlikeGif( gif.gifSrc ) }
 						>x</button>
 					}
 				</li>
