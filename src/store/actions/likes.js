7cdd2682ee7b6421b141dd0310ac0bd0d96d6a82
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 15:25:39 -1000

    dispatch redux-form reset and displaysearchresult actions when user likes a gif, resetting the form and prompting user to search for a new gif

--- src/store/actions/likes.js
+++ src/store/actions/likes.js
@@ -1,14 +1,18 @@
 import { ADD_GIF, REMOVE_GIF, RESET_LIKES } from './actionTypes';
 
+//actions
+import { displaySearchResult } from './search';
+import { reset } from 'redux-form';
+
 export const addGifToLikes = gif => ({
 	type: ADD_GIF,
 	gif
 });
 
 export const likeGif = gif => dispatch => {
-	return(
-		dispatch( addGifToLikes( gif ) )
-	)
+	dispatch( addGifToLikes( gif ) );
+	dispatch( reset( 'searchGifs' )); //Reset form
+	dispatch( displaySearchResult( '' )); //Reset search term to display, prompting user to search again
 }
 
 export const removeGifFromLikes = gifSrc => ({
