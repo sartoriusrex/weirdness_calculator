68f9dfe8b5873ad9e9fd7868a167d75a8b7ee443
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 14:58:20 -1000

    document Likes component

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -13,6 +13,7 @@ const Likes = props => {
 			
 			<LikesList from="likes" />
 
+			{/* If the length of the likes list is not 5, then show user how many more gifs they need to like. Otherwise show them the link to calculate their results and navigate them to results page */}
 			{ likes.length !== 5 ? 
 				`You must like ${ 5 - likes.length } more GIFs to calculate your score.` 
 				:
