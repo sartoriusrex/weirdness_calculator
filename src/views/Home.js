ccacbb5ac8df3f84dae7c01c43991a9c7022d02e
sartoriusrex <dmai.developer@gmail.com>
2019-12-02 21:18:35 -1000

    style Home Page text

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,13 +4,18 @@ import SearchForm from '../components/SearchForm';
 import SearchResult from '../components/SearchResult';
 import LikesContainer from '../components/LikesContainer';
 
-const Home = () => {
+export default function Home() {
 	return(
-		<main className="container mx-auto">
-			<h1>Weirdness Calculator</h1>
-			<p>Find out how weird you are by selecting the GIFs that make you laugh. We will show the least weird ones to start. To change the weirdness of the results, you can move the slider.</p>
-			<p>When you find a GIF you like, press the <em>Like</em> button. Once you like 5 GIFs, you can calculate your weirdness.</p>
-			<p>You can only select one GIF per search term. For example, you cannot like 3 GIFs that you found by only searching for "pantomine." At any point, you can remove previously liked GIFs.</p>
+		<main className="flex flex-col mx-12">
+			<h1 className="text-4xl text-purple-800 text-center my-4">Weirdness Calculator</h1>
+
+			<section className="flex flex-col md:flex-row">
+				<p className="my-2 md:block md:w-1/3 md:px-2">Find out how weird you are by selecting the GIFs that make you laugh. We will show the least weird ones to start. To change the weirdness of the results, you can move the slider.</p>
+				<p className="my-2 md:block md:w-1/3 md:px-2">When you find a GIF you like, press the <em>Like</em> button. Once you like 5 GIFs, you can calculate your weirdness.</p>
+				<p className="my-2 md:block md:w-1/3 md:px-2">You can only select one GIF per search term. For example, you cannot like 3 GIFs that you found by only searching for "pantomine." At any point, you can remove previously liked GIFs.</p>
+			</section>
+
+			<hr className="my-4"/>
 
 			<SearchForm />
 
@@ -21,5 +26,3 @@ const Home = () => {
 		</main>
 	)
 }
-
-export default Home;
